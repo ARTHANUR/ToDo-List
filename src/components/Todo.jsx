76d9edec80e4dd43bb26125dff868cf3ece61db8
shashank <sarthanur@gmail.com>
2023-06-18 22:27:34 +0530

    styling in progress

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import {FaTrash ,FaPlus } from "react-icons/fa";
-import "./Todo.css"
-
-
+import { FaTrash, FaPlus, FaCheck } from "react-icons/fa";
+import "./Todo.css";
 
 const Todo = () => {
     const [input, setInput] = useState("");
@@ -26,23 +24,45 @@ const Todo = () => {
         }
     };
     const handleDelete = (index) => {
-        const updatedTodo = todo.filter((_,i) => i !== index);
-        setTodo(updatedTodo)
-    }
+        const updatedTodo = todo.filter((_, i) => i !== index);
+        setTodo(updatedTodo);
+    };
+
+    const generateRandomColor = () => {
+        const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        return randomColor;
+    };
+
+    // const handleCheck = (index) => {
+    //     const updatedTodo = todo.map((item, i) => {
+    //         if (i === index) {
+    //             return { ...item, completed: !item.completed };
+    //         }
+    //         return item;
+    //     });
+    //     setTodo(updatedTodo);
+    // };
+
     
+
     return (
         <>
-            <h1>What's On Your mind ?</h1>
+            <h1>Notes</h1>
             <div className="input-area">
                 <input onKeyPress={handleKeyPress} value={input} onChange={handleInput} type="text" />
-                {/* <button onClick={handleAddTodo}>+</button> */}
                 <FaPlus className="plus-icon" onClick={handleAddTodo} />
+                <h1>Clear all</h1>
             </div>
             <div className="container">
-                {todo.map((item , index) => (
-                    <div className="content">
-                        <h3 key={index}>{item}</h3>
-                        <FaTrash  onClick={ () => handleDelete(index)} />
+                {todo.map((item, index) => (
+                    <div style={{ backgroundColor: generateRandomColor() }} className="content">
+                        <h3 style={{ textDecoration: item.completed ? "line-through" : "none" }} key={index}>
+                            {item}
+                        </h3>
+                        <div className="edit-todo">
+                            <FaCheck className="check-icon icon" /*onClick={() => handleCheck(index)}*/ />
+                            <FaTrash className="delete-icon icon" onClick={() => handleDelete(index)} />
+                        </div>
                     </div>
                 ))}
             </div>
